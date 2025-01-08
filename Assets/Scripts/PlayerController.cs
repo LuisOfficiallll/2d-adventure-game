bde295257bfc647ec0d750a3e2aa87b818bb4707
LuisOfficiallll <Luiso127@nycstudents.net>
2025-01-08 09:05:51 -0500

    Part 12 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -34,10 +34,11 @@ public class PlayerController : MonoBehaviour
 
      }
 
-     void ChangeHealth(int amount)
+     public void ChangeHealth(int amount)
      {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
 
      }
 }
+    
